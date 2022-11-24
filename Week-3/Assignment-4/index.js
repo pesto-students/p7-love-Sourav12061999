d63bce165cde0c8112087d649218a6fb5c7541b1
Sourav Das <sdunity12061999@gmail.com>
2022-11-24 07:51:03 +0530

    Assignmnet-4 Done

---
+++ Week-3/Assignment-4/index.js
@@ -0,0 +1,15 @@
+function createStack() {
+  const items = [];
+  return {
+    push(item) {
+      items.push(item);
+    },
+    pop() {
+      return items.pop();
+    },
+  };
+}
+const stack = createStack();
+stack.push(10);
+stack.push(5);
+stack.pop();
