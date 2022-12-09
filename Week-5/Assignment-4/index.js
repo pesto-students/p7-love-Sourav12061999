6c8213cc3c10eff0107d4487c614f1e96f1eb249
Sourav Das <sdunity12061999@gmail.com>
2022-12-09 08:34:04 +0530

    Assignment-4

---
+++ Week-5/Assignment-4/index.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const minus = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const devide = (a, b) => {
+  return a / b;
+};
+
+module.exports = {
+  sum,
+  minus,
+  multiply,
+  devide,
+};
