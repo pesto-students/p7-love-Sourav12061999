c25ffa34912f3080c53a64f661e4376056cf99e0
Sourav Das <sdunity12061999@gmail.com>
2023-01-08 20:11:14 +0530

    Reverse Linkedlist Complete

--- Week-8/ques.js
+++ Week-8/ques.js
@@ -1,53 +1,29 @@
-// Reverse a linkedlist
-class Node {
-  constructor(value, prev, next) {
-    this.value = value;
-    (this.prev = prev || null), (this.next = next || null);
-  }
-}
+// Reverse a Linkedlist
 
-class Linkedlist {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  isEmpty() {
-    return this.head === null;
-  }
-  add(value) {
-    const newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.head = newNode;
-    } else {
-      this.tail.next = newNode;
-    }
-
-    this.tail = newNode;
-    this.length++;
-  }
-  #loop(node, callback = () => {}) {
-    if (node.next === null) {
-      callback(node);
-      return;
-    }
-    callback(node);
-    this.#loop(node.next, callback);
-  }
-  print() {
-    if (this.isEmpty()) {
-      return [];
-    }
-    const elements = [];
-    this.#loop(this.head, (node) => {
-      elements.push(node.value);
-    });
-    return elements;
-  }
-}
+const { Linkedlist } = require("./Mod/linkedlist");
 
 const linkedlist = new Linkedlist();
+
 linkedlist.add(10);
 linkedlist.add(20);
 linkedlist.add(30);
+linkedlist.add(40);
+linkedlist.add(50);
+linkedlist.add(60);
+// console.log(linkedlist);
+// console.log(linkedlist.print());
+
+const reverseNode = (node) => {
+  const prev = node.prev;
+  node.prev = node.next;
+  node.next = prev;
+};
+const reverseLinkedlist = (linkedlist) => {
+  linkedlist.loop(linkedlist.head, reverseNode, "prev");
+  const tail = linkedlist.tail;
+  linkedlist.tail = linkedlist.head;
+  linkedlist.head = tail;
+};
+
+reverseLinkedlist(linkedlist);
 console.log(linkedlist.print());
